265d9eb36a55ba080d4f256d03e471a9c40a5eae
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 13:41:32 -0300

    Create fahrenheit-celsius.py

---
+++ fahrenheit-celsius.py
@@ -0,0 +1,6 @@
+# Faça um programa em Python que converta a temperatura de Fahrenheit para Celsius.
+temperaturafahrenheit=input("Informe uma temperatura em Fahrenheit: ")
+temperaturacelsius=(float(temperaturafahrenheit)-32)*5/9
+print("A temperatura em Celsius é: ",temperaturacelsius)
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
